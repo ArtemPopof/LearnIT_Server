9deed6b9ecc1724160998a577d52331da6b6c9c6
Artem Popov <Deviling42@gmail.com>
2020-04-20 23:02:17 +0300

    init commit

---
+++ src/main/kotlin/ru/abbysoft/learnit/server/ServerApplication.kt
@@ -0,0 +1,11 @@
+package ru.abbysoft.learnit.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServerApplication>(*args)
+}
---
+++ src/test/kotlin/ru/abbysoft/learnit/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.abbysoft.learnit.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
